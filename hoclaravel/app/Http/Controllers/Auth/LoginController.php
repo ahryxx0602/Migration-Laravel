3473c5bfd14ce86c85e56ffe0a3038ce6caf4ab8
Ahryxx <phvanthanh06@gmail.com>
2025-10-25 10:09:59 +0700

    custom đăng nhập

--- hoclaravel/app/Http/Controllers/Auth/LoginController.php
+++ hoclaravel/app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
 
 class LoginController extends Controller
 {
@@ -37,4 +40,35 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    protected function validateLogin(Request $request)
+    {
+        $request->validate(
+            [
+                $this->username() => 'required|string|email',
+                'password' => 'required|string|min:6',
+            ],
+            [
+                $this->username() . '.required' => 'Tên đăng nhập bắt buộc phải nhập',
+                $this->username() . 'string' => 'Tên đăng nhập không hợp lệ',
+                $this->username() . 'email' => 'Tên đăng nhập phải đúng định dạng email',
+
+                'password.required' => 'Mật khẩu bắt buộc phải nhập',
+                'password.string' => 'Kiểu dữ liệu mật khẩu không hợp lệ',
+                'password.min' => 'Mật khẩu phải lớn hơn :min kí tự',
+            ]
+        );
+    }
+
+    protected function sendFailedLoginResponse(Request $request)
+    {
+        throw ValidationException::withMessages([
+            $this->username() => ['Tên đăng nhập hoặc mật khẩu không hợp lệ'],
+        ]);
+    }
+
+    // public function username()
+    // {
+    //     return 'username';
+    // }
 }
